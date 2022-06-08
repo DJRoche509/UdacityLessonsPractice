e775587434180790d30deb26ba3de890dd4dc600
DJRoche509 <davjroche@gmail.com>
2022-06-08 02:15:36 -0400

    initial commit

---
+++ turtleTest.py
@@ -0,0 +1,13 @@
+import turtle
+dave = turtle.Turtle()
+roche = turtle.Turtle()
+dave.color("red")
+dave.forward(100)
+dave.right(135)
+dave.forward(140)
+dave.right(135)
+dave.forward(200)
+dave.left(135)
+dave.forward(140)
+dave.left(135)
+dave.forward(100)
